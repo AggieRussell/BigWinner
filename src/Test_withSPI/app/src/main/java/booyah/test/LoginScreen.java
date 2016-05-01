ffe201ffc69ed068f30ea7478d5967b1122eac51
maeii426 <maeii426@tamu.edu>
2016-05-01 11:22:09 -0500

    predictor implemented- pi rating .txt file not yet made

--- src/Test_withSPI/app/src/main/java/booyah/test/LoginScreen.java
+++ src/Test_withSPI/app/src/main/java/booyah/test/LoginScreen.java
@@ -33,12 +33,15 @@ public class LoginScreen extends Activity {
         final TableLayout lm = (TableLayout) findViewById(R.id.tableLogin);
         final TextView user = (TextView) findViewById(R.id.username);
         final Spinner spinner = (Spinner) findViewById(R.id.seasonSpinner);
+        final Spinner spinner2 = (Spinner) findViewById(R.id.predictorSpinner);
         final Button login = (Button) findViewById(R.id.loginButton);
         final Button instruct = (Button) findViewById(R.id.instructButton);
         final TextView msg = (TextView) findViewById(R.id.afterLogin);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.season_array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.predictor_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        spinner2.setAdapter(adapter2);
 
         final NFLPresenter p = (NFLPresenter) getApplicationContext();
 
@@ -46,7 +49,8 @@ public class LoginScreen extends Activity {
             public void onClick(View v) {
                 String name = user.getText().toString();
                 String year = spinner.getSelectedItem().toString();
-            //    msg.setText(s);
+                String algorithm = spinner2.getSelectedItem().toString();
+                //    msg.setText(s);
 
                 if (name.equals("")) {
                     Toast toast = Toast.makeText(getApplicationContext(), "Please enter a username", Toast.LENGTH_SHORT);
@@ -54,7 +58,7 @@ public class LoginScreen extends Activity {
                     toast.show();
                 }
                 else {
-                    p.setM(name,year);
+                    p.setM(name, year, algorithm);
                     p.resetWeek();
                     whistleSound.start();
 
